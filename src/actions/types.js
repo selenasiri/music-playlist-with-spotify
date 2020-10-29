90b7ab837ab891126d5f6d7474fa5673364bad40
Selena Siri <selenasiri@gmail.com>
2020-10-29 13:16:16 -0400

    artistReducer

---
+++ src/actions/types.js
@@ -0,0 +1,6 @@
+// Create actions here
+export const GET_ARTIST = 'GET_ARTIST';
+export const ARTIST_ERROR = 'ARTIST_ERROR';
+export const PLAY_TRACK = 'PLAY_TRACK';
+export const TOGGLE_PLAY = 'TOGGLE_PLAY';
+export const RESET_AUDIO = 'RESET_AUDIO';
