94832ff0617a8598e420b9a900bdc68cfbf93ed6
Selena Siri <selenasiri@gmail.com>
2020-10-29 13:29:59 -0400

    index page

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// No redux, just place App in here:
+import React from 'react';
+import ReactDOM from 'react-router-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
