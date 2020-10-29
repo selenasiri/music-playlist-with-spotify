7f0de1988dd7dcce73ca816c60b2582c63b2417c
Selena Siri <selenasiri@gmail.com>
2020-10-29 12:26:41 -0400

    root reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import artistReducer from './artistReducer'
+
+const rootReducer = combineReducers({
+  artist: artistReducer
+})
+
+export default rootReducer
