f1dc1116faf0b7881a5bce051fbe7fbd293de05c
Selena Siri <selenasiri@gmail.com>
2020-10-29 13:32:18 -0400

    fixed import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // No redux, just place App in here:
 import React from 'react';
-import ReactDOM from 'react-router-dom';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
