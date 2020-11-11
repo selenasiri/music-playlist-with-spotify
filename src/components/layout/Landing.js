c3da1763858a487d70b5e15e2bbfbbb6f8d73ac3
Selena Siri <selenasiri@gmail.com>
2020-11-11 12:02:24 -0500

    finished css

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -21,20 +21,11 @@ const Landing = () => {
       </header>
 
       <section class="my-5">
-        <div class="container">
+        <div class="container"> 
           <div class="row">
-            {/* <div class="col-md-8 mx-auto">
-              <p>The HTML5 video element uses an mp4 video as a source. Change the source video to add in your own background! The header text is vertically centered using flex utilities that are build into Bootstrap 4.</p>
-              <p>The overlay color can be changed by changing the <code>background-color</code> of the <code>.overlay</code> class in the CSS.</p>
-              <p>Set the mobile fallback image in the CSS by changing the background image of the header element within the media query at the bottom of the CSS snippet.</p>
-              <p class="mb-0">
-                Created by <a href="https://startbootstrap.com">Start Bootstrap</a>
-              </p>
-            </div> */}
-
             <div className="genre">
               <p className="lead">
-                Select a genre 
+                Select a genre:
               </p>
               <div className="buttons">
                 <Link to="/opera" className="btn btn-primary">
